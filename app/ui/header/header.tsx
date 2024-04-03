4bc6d2b6b69b13321c21420b57cfa22a5ee020b3
Gutu Corneliu <sprintyx@gmail.com>
2024-04-03 16:08:11 +0300

    setted eslint

--- app/ui/header/header.tsx
+++ app/ui/header/header.tsx
@@ -1,7 +1,29 @@
 export default function HeaderMain() {
-    return (
-        <header className="bg-bgColor text-white flex items-center justifiy center h-16">
-            This is Header!
-        </header>
-    )
+  return (
+    <header className="bg-bgColor text-white flex items-center justifiy center h-16">
+      <ul className="flex gap-7">
+        <li>
+          <HeaderComponents title="Noutati" />
+        </li>
+        <li>
+          <HeaderComponents title="Evenimente" />
+        </li>
+        <li>
+          <HeaderComponents title="Documente" />
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+type HeaderComponentsProps = {
+  title: string;
+};
+
+function HeaderComponents({ title }: HeaderComponentsProps) {
+  return (
+    <section>
+      <p>{title}</p>
+    </section>
+  );
 }
